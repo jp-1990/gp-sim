cb17d15efbf03e2f623da25e55dbba9bdbacb55c
James Plummer <plum_1990@hotmail.co.uk>
2022-04-04 22:38:54 +0100

    set up testing mocks for auth0

--- src/utils/testing/test-utils.tsx
+++ src/utils/testing/test-utils.tsx
@@ -1,9 +1,44 @@
 import React, { useMemo } from 'react';
 import { render, RenderOptions } from '@testing-library/react';
 import { IntlProvider } from 'react-intl';
+import { ChakraProvider } from '@chakra-ui/react';
+import { UserProvider, useUser } from '@auth0/nextjs-auth0';
+
+import theme from '../../styles/chakra-theme';
 
 import English from '../../../lang/en.json';
 import French from '../../../lang/fr.json';
+
+interface Auth0User {
+  name?: string;
+  nickname?: string;
+  picture?: string;
+  sub?: string;
+  updated_at?: string;
+}
+const testUser: Record<'user', Auth0User | undefined> = {
+  user: {
+    name: 'test-name',
+    nickname: 'test-nickname',
+    picture: 'test-image-url',
+    sub: 'test-sub',
+    updated_at: 'test-updated-at'
+  }
+};
+jest.mock('@auth0/nextjs-auth0', () => ({
+  UserProvider: ({ children }: any) => <>{children}</>,
+  withAuthenticationRequired: (component: any, _: any) => component,
+  useUser: () => {
+    return {
+      isLoading: false,
+      user: testUser.user,
+      isAuthenticated: true,
+      loginWithRedirect: jest.fn()
+    };
+  }
+}));
+export const setTestUser = (user: Auth0User | undefined) =>
+  (testUser.user = user);
 
 interface Props {
   locale: string;
@@ -21,9 +56,13 @@ const TestProviders: React.FC<Props> = ({ children, locale = 'en' }) => {
   }, [locale]);
 
   return (
-    <IntlProvider messages={messages} locale={locale} defaultLocale={'en'}>
-      {children}
-    </IntlProvider>
+    <UserProvider>
+      <ChakraProvider theme={theme}>
+        <IntlProvider messages={messages} locale={locale} defaultLocale={'en'}>
+          {children}
+        </IntlProvider>
+      </ChakraProvider>
+    </UserProvider>
   );
 };
 
