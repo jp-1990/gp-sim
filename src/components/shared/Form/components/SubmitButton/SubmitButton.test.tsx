cff8a473035ad9a5a5397f2b6e1255b195819206
James Plummer <plum_1990@hotmail.co.uk>
2022-04-16 14:31:20 +0100

    added custom form submit button and tests

---
+++ src/components/shared/Form/components/SubmitButton/SubmitButton.test.tsx
@@ -0,0 +1,152 @@
+import '@testing-library/jest-dom';
+import {
+  render,
+  screen,
+  setTestUser
+} from '../../../../../utils/testing/test-utils';
+import userEvent from '@testing-library/user-event';
+
+import SubmitButton from './SubmitButton';
+import { Form, initialFormState, FORM_CONTEXT_ERROR } from '../../Form';
+
+describe('SubmitButton', () => {
+  beforeEach(() => {
+    setTestUser(undefined);
+    jest.spyOn(console, 'error').mockImplementation(() => {});
+    jest.spyOn(console, 'warn').mockImplementation(() => {});
+  });
+  afterAll(() => jest.clearAllMocks());
+
+  const TESTING_TEXT = 'testing-label';
+  it('renders the text on the button', () => {
+    render(
+      <Form>
+        <SubmitButton onClick={() => null}>{TESTING_TEXT}</SubmitButton>
+      </Form>
+    );
+    expect(screen.getByText(TESTING_TEXT)).toBeInTheDocument();
+  });
+
+  it('fires the provided function onClick', async () => {
+    const user = userEvent.setup();
+    const onClickMock = jest.fn();
+    render(
+      <Form>
+        <SubmitButton onClick={onClickMock}>{TESTING_TEXT}</SubmitButton>
+      </Form>
+    );
+    const button = screen.getByRole('button');
+    expect(screen.getByText(TESTING_TEXT)).toBeInTheDocument();
+    await user.click(button);
+    expect(onClickMock).toHaveBeenCalledWith(
+      initialFormState,
+      expect.anything()
+    );
+  });
+
+  it('fires the provided function onClick which can adjust form status to loading', async () => {
+    const user = userEvent.setup();
+    const onClickMock = jest.fn((_, setFormStatus) => {
+      setFormStatus('loading', true);
+    });
+    render(
+      <Form>
+        <SubmitButton onClick={onClickMock}>{TESTING_TEXT}</SubmitButton>
+      </Form>
+    );
+    const button = screen.getByRole('button');
+    expect(screen.getByText(TESTING_TEXT)).toBeInTheDocument();
+    await user.click(button);
+    expect(onClickMock).toHaveBeenCalledWith(
+      initialFormState,
+      expect.anything()
+    );
+    expect(screen.queryByText(TESTING_TEXT)).not.toBeInTheDocument();
+  });
+
+  it('fires the provided function onClick which can adjust form status isValid to disable submit', async () => {
+    const user = userEvent.setup();
+    const onClickMock = jest.fn((_, setFormStatus) => {
+      setFormStatus('isValid', false);
+    });
+    render(
+      <Form>
+        <SubmitButton onClick={onClickMock}>{TESTING_TEXT}</SubmitButton>
+      </Form>
+    );
+    const button = screen.getByRole('button');
+    expect(screen.getByText(TESTING_TEXT)).toBeInTheDocument();
+    await user.click(button);
+    expect(onClickMock).toHaveBeenCalledWith(
+      initialFormState,
+      expect.anything()
+    );
+    expect(screen.queryByText(TESTING_TEXT)).toBeInTheDocument();
+    await user.click(button);
+    expect(button).toBeDisabled();
+    expect(onClickMock).toHaveBeenCalledTimes(1);
+  });
+
+  it('fires the provided function onClick which can adjust form status error to disable submit', async () => {
+    const user = userEvent.setup();
+    const onClickMock = jest.fn((_, setFormStatus) => {
+      setFormStatus('error', true);
+    });
+    render(
+      <Form>
+        <SubmitButton onClick={onClickMock}>{TESTING_TEXT}</SubmitButton>
+      </Form>
+    );
+    const button = screen.getByRole('button');
+    expect(screen.getByText(TESTING_TEXT)).toBeInTheDocument();
+    await user.click(button);
+    expect(onClickMock).toHaveBeenCalledWith(
+      initialFormState,
+      expect.anything()
+    );
+    expect(screen.queryByText(TESTING_TEXT)).toBeInTheDocument();
+    await user.click(button);
+    expect(button).toBeDisabled();
+    expect(onClickMock).toHaveBeenCalledTimes(1);
+  });
+
+  it('cannot be clicked when loading', async () => {
+    const user = userEvent.setup();
+    const onClick = jest.fn();
+    render(
+      <Form>
+        <SubmitButton isLoading={true} onClick={onClick}>
+          {TESTING_TEXT}
+        </SubmitButton>
+      </Form>
+    );
+    const button = screen.getByRole('button');
+    expect(screen.queryByText(TESTING_TEXT)).not.toBeInTheDocument();
+    await user.click(button);
+    expect(onClick).not.toHaveBeenCalledWith(initialFormState);
+  });
+
+  it('cannot be clicked when disabled', async () => {
+    const user = userEvent.setup();
+    const onClick = jest.fn();
+    render(
+      <Form>
+        <SubmitButton isDisabled={true} onClick={onClick}>
+          {TESTING_TEXT}
+        </SubmitButton>
+      </Form>
+    );
+    const button = screen.getByRole('button');
+    expect(screen.queryByText(TESTING_TEXT)).toBeInTheDocument();
+    await user.click(button);
+    expect(onClick).not.toHaveBeenCalled();
+  });
+
+  it('does not render outside of the Form component', () => {
+    try {
+      render(<SubmitButton onClick={() => null}>{TESTING_TEXT}</SubmitButton>);
+    } catch (err: any) {
+      expect(err.message).toEqual(FORM_CONTEXT_ERROR);
+    }
+  });
+});
---
+++ src/components/shared/Form/components/SubmitButton/SubmitButton.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import {
+  ComponentWithAs,
+  Button as ChakraButton,
+  ButtonProps as ChakraButtonProps
+} from '@chakra-ui/react';
+import { FormattedMessage } from 'react-intl';
+
+import { useForm } from '../../Form';
+import { submitButtonSubmitForm, setFormStatus } from '../../utils';
+import { FormStateType, FormValueType, SetFormStatusType } from '../../types';
+import { commonStrings } from '../../../../../utils/intl';
+
+interface SubmitButtonProps {
+  loadingText?: React.ReactNode | string;
+  onClick: (state: FormStateType, setFormStatus: SetFormStatusType) => void;
+}
+/**
+ * Accepts any props accepted by ChakraUI Button. Submit action must be handled by passing an onClick function as props with a single 'state' argument.
+ * @returns Function Component
+ */
+const SubmitButton: ComponentWithAs<
+  'button',
+  Omit<ChakraButtonProps, 'onClick'> & SubmitButtonProps
+> = ({ loadingText, onClick, children, ...chakraProps }) => {
+  const { state, setState } = useForm();
+  const submitDisabled = !state.isValid || state.error;
+
+  const isLoadingState = chakraProps.isLoading ?? state.loading;
+  const isLoadingTextString =
+    loadingText ??
+    ((<FormattedMessage {...commonStrings.uploading} />) as unknown as string);
+
+  const isDisabledState = chakraProps.isDisabled ?? submitDisabled;
+  const onSubmit = submitButtonSubmitForm(
+    state,
+    setFormStatus(setState),
+    onClick ?? (() => null)
+  );
+  return (
+    <ChakraButton
+      test-id={`form-submit-button`}
+      id={`form-submit-button`}
+      _focus={{
+        boxShadow: '0 0 1px 2px gray, 0 1px 1px gray'
+      }}
+      loadingText={isLoadingTextString}
+      isLoading={isLoadingState}
+      isDisabled={isDisabledState}
+      {...chakraProps}
+      onClick={onSubmit}
+    >
+      {children}
+    </ChakraButton>
+  );
+};
+
+export default SubmitButton;
