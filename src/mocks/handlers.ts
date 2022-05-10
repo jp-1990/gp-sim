ea620786f62a659f23ba8a4c7e6738896104be89
James Plummer <plum_1990@hotmail.co.uk>
2022-05-10 17:26:12 +0100

    added liveries handlers to root handlers

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,3 +1,4 @@
 import { carsHandlers } from './cars/handlers';
+import { liveriesHandlers } from './liveries/handlers';
 
-export const handlers = [...carsHandlers];
+export const handlers = [...carsHandlers, ...liveriesHandlers];
