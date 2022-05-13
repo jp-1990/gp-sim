c90e5841bd98308757eb29181fb4cb51b8e1a041
James Plummer <plum_1990@hotmail.co.uk>
2022-05-13 22:17:27 +0100

    updated test config to use msw

--- jest.setup.js
+++ jest.setup.js
@@ -1,3 +1,23 @@
 import "@testing-library/jest-dom";
+import { server } from './src/mocks/server';
+import next from 'next'
+import { loadEnvConfig } from '@next/env'
+
+next({})
+
+loadEnvConfig(process.cwd())
 
 window.URL.createObjectURL = jest.fn((file)=>`/${file.name}`)
+
+// Establish API mocking before all tests.
+beforeAll(() => server.listen({ onUnhandledRequest: 'error' }))
+// Reset any request handlers that we may add during the tests,
+beforeEach(()=> {
+    jest.resetModules()
+})
+// so they don't affect other tests.
+afterEach(() => {
+    server.resetHandlers()}
+    )
+// Clean up after the tests are finished.
+afterAll(() => server.close())
