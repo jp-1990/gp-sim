0fcd72c7b4b3269d44972ab21bba072ffc8b36be
James Plummer <plum_1990@hotmail.co.uk>
2022-05-14 22:51:36 +0100

    added headers option to base query

--- src/store/store.ts
+++ src/store/store.ts
@@ -14,13 +14,20 @@ const axiosBaseQuery =
       method: AxiosRequestConfig['method'];
       data?: AxiosRequestConfig['data'];
       params?: AxiosRequestConfig['params'];
+      headers?: AxiosRequestConfig['headers'];
     },
     unknown,
     unknown
   > =>
-  async ({ url, method, data, params }) => {
+  async ({ url, method, data, params, headers }) => {
     try {
-      const result = await axios({ url: baseUrl + url, method, data, params });
+      const result = await axios({
+        url: baseUrl + url,
+        method,
+        data,
+        params,
+        headers
+      });
       return { data: result.data };
     } catch (axiosError) {
       let err = axiosError as AxiosError;
