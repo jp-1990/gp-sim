7e629550552909e56bc5affa5f01ea57f9e748c5
James Plummer <plum_1990@hotmail.co.uk>
2022-05-15 19:00:41 +0100

    added toasts for success and error, rtk mutation for createLivery and resetState follow a non-erroring upload

--- src/components/features/liveries/CreateLivery/components/SubmitLivery/SubmitLivery.tsx
+++ src/components/features/liveries/CreateLivery/components/SubmitLivery/SubmitLivery.tsx
@@ -1,67 +1,81 @@
-/* eslint-disable @next/next/no-img-element */
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import { useUser } from '@auth0/nextjs-auth0';
+import { useToast } from '@chakra-ui/react';
 
 import { SubmitButton, useForm } from '../../../../../shared';
 import { liveryStrings } from '../../../../../../utils/intl';
-import { postLiveryThunk } from '../../../../../../store/livery/slice';
-import { useAppDispatch } from '../../../../../../store/store';
+import { useCreateLiveryMutation } from '../../../../../../store/livery/slice';
 import {
-  mapCreateLiveryFormStateToActionInput,
+  mapCreateLiveryFormStateToRequestInput,
   zipLiveryFiles
 } from '../../utils';
 import { CreateLiveryFormStateType } from '../../types';
+import { initialState } from '../../config';
 
 /**
- * Submit button for liveries/create page. Uses SubmitButton inside a form provider to dispatch an action to submit the state of the form.
+ * Submit button for liveries/create page. Uses SubmitButton inside a form provider to submit the state of the form.
  */
 const SubmitLivery = () => {
+  const toast = useToast({
+    duration: 8000,
+    isClosable: true,
+    position: 'top',
+    containerStyle: {
+      marginTop: '1.25rem'
+    }
+  });
   const { user } = useUser();
-  const { state, setStateImmutably } = useForm<CreateLiveryFormStateType>();
-  const dispatch = useAppDispatch();
+  const { state, resetState } = useForm<CreateLiveryFormStateType>();
+  const [createLivery, { isLoading }] = useCreateLiveryMutation();
 
   const onClick = async () => {
-    // prepare livery files
-    const liveryZip = await zipLiveryFiles({
-      folderName: state.title,
-      liveryFiles: state.liveryFiles
-    });
-
-    // prepare image files?
-    const imageFiles = state.imageFiles || [];
-
-    const createLiveryInput = {
-      // map state into upload format
-      ...mapCreateLiveryFormStateToActionInput({
-        formState: state,
-        user
-      }),
-      liveryZip,
-      imageFiles
-    };
-
-    setStateImmutably((prev) => {
-      prev.loading = true;
-      return prev;
-    });
     try {
-      dispatch(postLiveryThunk(createLiveryInput));
-      setStateImmutably((prev) => {
-        prev.loading = false;
-        return prev;
-      });
-    } catch (err) {
-      setStateImmutably((prev) => {
-        prev.loading = false;
-        prev.error = true;
-        return prev;
+      if (!isLoading) {
+        // prepare livery files
+        const liveryZip = await zipLiveryFiles({
+          folderName: state.title,
+          liveryFiles: state.liveryFiles
+        });
+
+        // prepare image files?
+        const imageFiles = state.imageFiles || [];
+
+        const createLiveryInput = {
+          // map state into upload format
+          ...mapCreateLiveryFormStateToRequestInput({
+            formState: state,
+            user
+          }),
+          liveryZip,
+          imageFiles
+        };
+
+        await createLivery(createLiveryInput).unwrap();
+        resetState(initialState);
+        toast({
+          title: 'Livery successfully created',
+          description: `${state.title}`,
+          status: 'success'
+        });
+      }
+    } catch (_) {
+      toast({
+        title: 'Something went wrong!',
+        description: `We're sorry, something went wrong when creating your livery`,
+        status: 'error'
       });
     }
   };
 
   return (
-    <SubmitButton onClick={onClick} colorScheme="red" w="2xs" lineHeight={1}>
+    <SubmitButton
+      onClick={onClick}
+      colorScheme="red"
+      w="2xs"
+      lineHeight={1}
+      isLoading={isLoading}
+    >
       {<FormattedMessage {...liveryStrings.uploadLivery} />}
     </SubmitButton>
   );
