b6b1c41ea4233afbc709f2e3757bd847f712e266
James Plummer <plum_1990@hotmail.co.uk>
2022-06-08 07:56:05 +0100

    garages index page with basic tests, garage select and livery filtering

---
+++ src/__tests__/pages/garages/index.test.tsx
@@ -0,0 +1,34 @@
+import '@testing-library/jest-dom';
+import { render, screen, waitFor } from '../../../utils/testing/test-utils';
+import Garages from '../../../pages/garages/index';
+import liveriesData from '../../../utils/dev-data/liveries.json';
+import garagesData from '../../../utils/dev-data/garages.json';
+import usersData from '../../../utils/dev-data/users.json';
+
+const userData = usersData[0];
+const userGarages = userData.garages;
+const userLiveries = userData.liveries;
+
+const userLiveriesData = [...liveriesData].filter(({ id }) =>
+  userLiveries.includes(id)
+);
+const userGaragesData = [...garagesData].filter(({ id }) =>
+  userGarages.includes(id)
+);
+
+describe('Liveries', () => {
+  it('renders the garages and liveries from the user collection', async () => {
+    render(<Garages />);
+
+    await waitFor(() => {
+      userGaragesData.forEach((garage) => {
+        expect(screen.getAllByText(garage.creator.displayName)).toBeTruthy();
+        expect(screen.getAllByText(garage.title)).toBeTruthy();
+      });
+      userLiveriesData.forEach((livery) => {
+        expect(screen.getAllByText(livery.creator.displayName)).toBeTruthy();
+        expect(screen.getAllByText(livery.title)).toBeTruthy();
+      });
+    });
+  });
+});
--- src/pages/garages/index.tsx
+++ src/pages/garages/index.tsx
@@ -1,6 +1,7 @@
-import { ChangeEvent, useReducer } from 'react';
+import { ChangeEvent, useEffect, useReducer, useState } from 'react';
 import {
   chakra,
+  Box,
   InputGroup,
   InputLeftElement,
   Input,
@@ -8,19 +9,23 @@ import {
   Grid,
   GridItem,
   Button,
-  Flex
+  Flex,
+  Text,
+  Heading
 } from '@chakra-ui/react';
 import type { NextPage } from 'next';
+import Link from 'next/link';
 import { SearchIcon } from '@chakra-ui/icons';
 import { FormattedMessage, useIntl } from 'react-intl';
 
-import { apiSlice, wrapper } from '../../store/store';
-import { getLiveries, useGetLiveriesQuery } from '../../store/livery/slice';
-import { getCars, useGetCarsQuery } from '../../store/car/slice';
+import { apiSlice, wrapper, useAppSelector } from '../../store/store';
+import { getLiveries, useGetLiveriesQuery } from '../../store/livery/api-slice';
+import { getCars, useGetCarsQuery } from '../../store/car/api-slice';
 
 import { LiveryList } from '../../components/features';
 import { MainLayout } from '../../components/layout';
 import { PageHeading } from '../../components/shared';
+import { ImageWithFallback } from '../../components/core';
 
 import {
   GARAGES_URL,
@@ -28,10 +33,11 @@ import {
   LIVERY_CREATE_URL
 } from '../../utils/nav';
 import { liveryStrings, garageStrings, commonStrings } from '../../utils/intl';
-import Link from 'next/link';
-import { LiveriesFilterKeys, Order } from '../../types';
+import { LiveriesFilterKeys, Order, KeyValueUnionOf } from '../../types';
+import { useGetGaragesQuery } from '../../store/garage/api-slice';
 
 const initialState = {
+  ids: '',
   search: '',
   car: '',
   created: Order.ASC,
@@ -40,69 +46,89 @@ const initialState = {
   user: '0',
   page: 0
 };
+type FilterState = typeof initialState;
 
-type Action = { type: LiveriesFilterKeys; payload: any };
-const filtersReducer = (state: typeof initialState, action: Action) => {
-  switch (action.type) {
-    case LiveriesFilterKeys.SEARCH: {
-      return { ...state, search: action.payload };
-    }
-    case LiveriesFilterKeys.CAR: {
-      return { ...state, car: action.payload };
-    }
-    case LiveriesFilterKeys.CREATED: {
-      return { ...state, created: action.payload };
-    }
-    case LiveriesFilterKeys.RATING: {
-      return { ...state, rating: action.payload };
-    }
-    case LiveriesFilterKeys.GARAGES: {
-      return { ...state, garages: action.payload };
-    }
-    case LiveriesFilterKeys.USER: {
-      return { ...state, user: action.payload };
-    }
-    case LiveriesFilterKeys.PAGE: {
-      return { ...state, page: action.payload };
-    }
-    default:
-      return state;
-  }
+type Action = {
+  type: keyof FilterState;
+  payload: NonNullable<KeyValueUnionOf<FilterState>>;
+};
+const filtersReducer = (state: FilterState, action: Action): FilterState => {
+  return {
+    ...state,
+    [action.payload.key]: action.payload.value
+  };
 };
 
 const Garages: NextPage = () => {
   // STATE
   const [filters, dispatch] = useReducer(filtersReducer, initialState);
+  const [selectedGarage, setSelectedGarage] = useState<string>('');
 
   // HOOKS
   const intl = useIntl();
+  const currentUser = useAppSelector((state) => state.currentUserSlice);
 
   // QUERIES
   const { data: cars } = useGetCarsQuery();
   const { data: liveries } = useGetLiveriesQuery(filters);
+  const { data: garages } = useGetGaragesQuery({
+    ids: currentUser.garages.join('&')
+  });
+
+  /// EFFECTS
+  useEffect(() => {
+    dispatch({
+      type: 'ids',
+      payload: {
+        key: 'ids',
+        value:
+          garages?.entities[selectedGarage]?.liveries.join('&') ||
+          currentUser.liveries.join('&')
+      }
+    });
+  }, [currentUser.liveries, garages?.entities, selectedGarage]);
 
   // HANDLERS
   const onSearchChange = (event: ChangeEvent<HTMLInputElement>) => {
-    dispatch({ type: LiveriesFilterKeys.SEARCH, payload: event.target.value });
+    dispatch({
+      type: LiveriesFilterKeys.SEARCH,
+      payload: { key: LiveriesFilterKeys.SEARCH, value: event.target.value }
+    });
   };
   const onCarChange = (event: ChangeEvent<HTMLSelectElement>) => {
-    dispatch({ type: LiveriesFilterKeys.CAR, payload: event.target.value });
+    dispatch({
+      type: LiveriesFilterKeys.CAR,
+      payload: { key: LiveriesFilterKeys.CAR, value: event.target.value }
+    });
   };
   const onCreatedChange = (event: ChangeEvent<HTMLSelectElement>) => {
     const { value } = event.target;
     if (value !== Order.ASC && value !== Order.DESC) return;
-    dispatch({ type: LiveriesFilterKeys.CREATED, payload: value });
+    dispatch({
+      type: LiveriesFilterKeys.CREATED,
+      payload: { key: LiveriesFilterKeys.CREATED, value: value }
+    });
   };
   const onRatingChange = (event: ChangeEvent<HTMLSelectElement>) => {
-    dispatch({ type: LiveriesFilterKeys.RATING, payload: event.target.value });
+    dispatch({
+      type: LiveriesFilterKeys.RATING,
+      payload: { key: LiveriesFilterKeys.RATING, value: event.target.value }
+    });
   };
   const onPageChange = (page: number) => () => {
-    dispatch({ type: LiveriesFilterKeys.PAGE, payload: page });
+    dispatch({
+      type: LiveriesFilterKeys.PAGE,
+      payload: { key: LiveriesFilterKeys.PAGE, value: page }
+    });
   };
+  const onSelectGarage = (id: string | undefined) => () =>
+    setSelectedGarage(id || '');
 
   const pages = Array.from(
     Array(Math.ceil((liveries?.total || 1) / (liveries?.perPage || 1))).keys()
   );
+
+  const highlightedColor = 'red';
 
   return (
     <MainLayout
@@ -130,7 +156,199 @@ const Garages: NextPage = () => {
           </Link>
         </Button>
       </Flex>
-      <chakra.section pt={8} w="5xl" display="flex" justifyContent="flex-start">
+
+      {garages && (
+        <chakra.section
+          pt={8}
+          w="5xl"
+          display="flex"
+          justifyContent="flex-start"
+          overflow="auto"
+        >
+          <Box
+            as="button"
+            onClick={onSelectGarage('')}
+            key="userCollection"
+            mb={2}
+            mx={0.5}
+            position="relative"
+            borderWidth="2px"
+            borderRadius={6}
+            borderColor={
+              selectedGarage === '' ? highlightedColor : 'blackAlpha.100'
+            }
+            overflow="hidden"
+            minW="3xs"
+          >
+            <Grid
+              templateColumns="repeat(6, 1fr)"
+              templateRows="repeat(2, minmax(5rem, auto))"
+              p={2}
+              zIndex="1"
+              position="relative"
+              top="0"
+              left="0"
+            >
+              <GridItem colSpan={6} rowSpan={1}>
+                <Flex
+                  direction="column"
+                  alignItems="center"
+                  justifyContent="flex-start"
+                  h="full"
+                  maxW="15rem"
+                  overflow="hidden"
+                >
+                  <Text
+                    color={'white'}
+                    borderRadius={5}
+                    px={2}
+                    bg={
+                      selectedGarage === ''
+                        ? highlightedColor
+                        : 'blackAlpha.800'
+                    }
+                    fontSize="sm"
+                    isTruncated
+                    noOfLines={1}
+                    w="full"
+                    textAlign="center"
+                  >
+                    <FormattedMessage {...garageStrings.yourCollection} />
+                  </Text>
+                </Flex>
+              </GridItem>
+              <GridItem colSpan={6} rowSpan={1}></GridItem>
+            </Grid>
+            <ImageWithFallback
+              imgAlt="user livery collection"
+              imgUrl={''}
+              position="absolute"
+              top="0"
+              left="0"
+              zIndex="0"
+              bg="gray.200"
+              h="full"
+              w="full"
+              borderRadius={3}
+            />
+          </Box>
+          {garages.ids.map((id) => (
+            <Box
+              as="button"
+              onClick={onSelectGarage(garages.entities[id]?.id)}
+              key={id}
+              mb={2}
+              mx={0.5}
+              position="relative"
+              borderWidth="2px"
+              borderRadius={6}
+              borderColor={
+                id === selectedGarage ? highlightedColor : 'blackAlpha.100'
+              }
+              overflow="hidden"
+              minW="3xs"
+            >
+              <Grid
+                templateColumns="repeat(6, 1fr)"
+                templateRows="repeat(2, minmax(5rem, auto))"
+                p={2}
+                zIndex="1"
+                position="relative"
+                top="0"
+                left="0"
+              >
+                <GridItem colSpan={6} rowSpan={1}>
+                  <Flex
+                    direction="column"
+                    alignItems="center"
+                    justifyContent="flex-start"
+                    h="full"
+                    maxW="15rem"
+                    overflow="hidden"
+                  >
+                    <Text
+                      color={'white'}
+                      borderRadius={5}
+                      px={2}
+                      bg={
+                        id === selectedGarage
+                          ? highlightedColor
+                          : 'blackAlpha.800'
+                      }
+                      fontSize="sm"
+                      isTruncated
+                      noOfLines={1}
+                      w="full"
+                      textAlign="center"
+                    >
+                      {garages.entities[id]?.title}
+                    </Text>
+                  </Flex>
+                </GridItem>
+                <GridItem colSpan={6} rowSpan={1}>
+                  <Flex
+                    direction="column"
+                    alignItems="flex-start"
+                    justifyContent="flex-end"
+                    h="full"
+                    maxW="15rem"
+                    overflow="hidden"
+                  >
+                    <Text
+                      color={'white'}
+                      borderRadius={5}
+                      px={2}
+                      bg={
+                        id === selectedGarage
+                          ? highlightedColor
+                          : 'blackAlpha.800'
+                      }
+                      fontSize="sm"
+                      isTruncated
+                      noOfLines={1}
+                    >
+                      {garages.entities[id]?.creator.displayName}
+                    </Text>
+                  </Flex>
+                </GridItem>
+              </Grid>
+              <ImageWithFallback
+                imgAlt={garages.entities[id]?.title}
+                imgUrl={garages.entities[id]?.image}
+                position="absolute"
+                top="0"
+                left="0"
+                zIndex="0"
+                bg="gray.200"
+                h="full"
+                w="full"
+                borderRadius={3}
+              />
+            </Box>
+          ))}
+        </chakra.section>
+      )}
+
+      <chakra.section
+        w="5xl"
+        pt={8}
+        display="flex"
+        flexDirection="column"
+        justifyContent="flex-start"
+      >
+        <Heading size="md" pb={4}>
+          {garages?.entities[selectedGarage]?.title ?? (
+            <FormattedMessage {...garageStrings.yourCollection} />
+          )}
+        </Heading>
+        <Text fontSize="sm" pb={4}>
+          {garages?.entities[selectedGarage]?.description ?? (
+            <FormattedMessage {...garageStrings.yourCollectionDescription} />
+          )}
+        </Text>
+      </chakra.section>
+
+      <chakra.section pt={4} w="5xl" display="flex" justifyContent="flex-start">
         <Grid
           templateColumns="repeat(9, 1fr)"
           templateRows="repeat(1, 1fr)"
