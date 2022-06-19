553177cb81c11e6441fb3fc7a0843bdb3e23c09e
James Plummer <plum_1990@hotmail.co.uk>
2022-06-19 11:29:53 +0100

    added types and utils for table component

---
+++ src/components/shared/Table/types/index.ts
@@ -0,0 +1,27 @@
+import { ReactNode } from 'react';
+
+export enum TableDataTypes {
+  STRING = 'string',
+  IMAGE = 'image',
+  ACTIONS = 'actions',
+  CHECKBOX = 'checkbox'
+}
+export type TableData = (Record<string | number, any> & {
+  id: string | number;
+})[];
+
+export type TableAction<T> = (props: T) => JSX.Element;
+
+export type TableActions<T extends TableData> = TableAction<T[number]>[];
+
+export interface TableColumn {
+  label: ReactNode;
+  dataKey: string;
+  type: TableDataTypes;
+}
+export type TableColumns = TableColumn[];
+
+export type TableSelected<T extends TableData> = T[number]['id'][];
+export type TableOnSelect<T extends TableData> = (
+  id: T[number]['id'] | T[number]['id'][]
+) => void;
--- src/components/shared/Table/utils/index.ts
+++ src/components/shared/Table/utils/index.ts
@@ -1,3 +1,10 @@
+import {
+  TableData,
+  TableDataTypes,
+  TableOnSelect,
+  TableSelected
+} from '../types';
+
 /**
  * Recursively access a nested property within an object.
  * @param {string} key - path to data within the row object (e.g. 'res.data.id').
@@ -16,4 +23,37 @@ export const accessRowDataByKey = <T extends Record<string, any>>(
     path.join('.'),
     row[nextKey]
   );
+};
+
+/**
+ * Wrapper around the provided onSelect function to toggle all ids selected
+ * @param {TableData} data - array of data objects. as a minimum, each object must must contain id: string | number
+ * @param {string[]} selected - array of currently selected ids
+ * @param {TableOnSelect} onSelect - function to add or removed ids from selected
+ */
+export const onSelectAll = <T extends TableData>(
+  data: T,
+  selected: TableSelected<T>,
+  onSelect: TableOnSelect<T>
+) => {
+  if (selected.length) return onSelect([]);
+  return onSelect(data.map(({ id }) => id));
+};
+
+/**
+ * Helper function to determine the number of columns a cell should span based on the data key provided
+ * @param {TableDataTypes.ACTIONS | TableDataTypes.CHECKBOX | string} dataKey - data key for the target column
+ * @returns 1 | 3 | 3 based on dataKey input
+ */
+export const getColSpan = (
+  dataKey: TableDataTypes.ACTIONS | TableDataTypes.CHECKBOX | string
+) => {
+  switch (dataKey) {
+    case TableDataTypes.CHECKBOX:
+      return 1;
+    case TableDataTypes.ACTIONS:
+      return 3;
+    default:
+      return 3;
+  }
 };
