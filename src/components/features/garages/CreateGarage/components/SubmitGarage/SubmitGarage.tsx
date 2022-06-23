3b8b0e32ee62c7ab1a393b0b1465b087f5bb5f50
James Plummer <plum_1990@hotmail.co.uk>
2022-06-23 08:14:26 +0100

    submit functionality for create garage

---
+++ src/components/features/garages/CreateGarage/components/SubmitGarage/SubmitGarage.tsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import { FormattedMessage, useIntl } from 'react-intl';
+import { useUser } from '@auth0/nextjs-auth0';
+import { useToast } from '@chakra-ui/react';
+
+import { SubmitButton, useForm } from '../../../../../shared';
+import {
+  commonStrings,
+  formStrings,
+  garageStrings
+} from '../../../../../../utils/intl';
+
+import { useCreateGarageMutation } from '../../../../../../store/garage/api-slice';
+
+import { initialState } from '../../config';
+import { CreateGarageFormStateType } from '../../types';
+import { mapCreateGarageFormStateToRequestInput } from '../../utils';
+
+/**
+ * Submit button for liveries/create page. Uses SubmitButton inside a form provider to submit the state of the form.
+ */
+const SubmitLivery = () => {
+  const intl = useIntl();
+  const toast = useToast({
+    duration: 8000,
+    isClosable: true,
+    position: 'top',
+    containerStyle: {
+      marginTop: '1.25rem'
+    }
+  });
+  const { user } = useUser();
+  const { state, resetState } = useForm<CreateGarageFormStateType>();
+  const [createGarage, { isLoading }] = useCreateGarageMutation();
+
+  const onClick = async () => {
+    try {
+      if (!isLoading) {
+        // prepare image files?
+        const imageFiles = state.imageFiles || [];
+
+        const createGarageInput = {
+          // map state into upload format
+          ...mapCreateGarageFormStateToRequestInput({
+            formState: state,
+            user
+          }),
+          imageFiles
+        };
+
+        await createGarage(createGarageInput).unwrap();
+        resetState(initialState);
+        toast({
+          title: intl.formatMessage(formStrings.createSuccess, {
+            item: intl.formatMessage(commonStrings.garage)
+          }),
+          description: `${state.title}`,
+          status: 'success'
+        });
+      }
+    } catch (_) {
+      toast({
+        title: intl.formatMessage(commonStrings.error),
+        description: intl.formatMessage(formStrings.createError, {
+          item: intl.formatMessage(commonStrings.garage)
+        }),
+        status: 'error'
+      });
+    }
+  };
+
+  return (
+    <SubmitButton
+      onClick={onClick}
+      colorScheme="red"
+      w="2xs"
+      lineHeight={1}
+      isLoading={isLoading}
+    >
+      {<FormattedMessage {...garageStrings.createGarage} />}
+    </SubmitButton>
+  );
+};
+
+export default SubmitLivery;
---
+++ src/components/features/garages/CreateGarage/utils/functions.ts
@@ -0,0 +1,35 @@
+import { UserProfile } from '@auth0/nextjs-auth0';
+
+import { FormStateType } from '../../../../shared/Form/types';
+import { CreateGarageFormStateType } from '../types';
+import { CreateGarageDataType } from '../../../../../types';
+
+interface MapCreateGarageFormStateToRequestInputArgs {
+  formState: FormStateType<CreateGarageFormStateType>;
+  user: UserProfile | undefined;
+}
+export const mapCreateGarageFormStateToRequestInput = ({
+  formState,
+  user
+}: MapCreateGarageFormStateToRequestInputArgs): Omit<
+  CreateGarageDataType,
+  'imageFiles'
+> => {
+  if (process.env.NODE_ENV === 'development') {
+    user = {
+      sub: `${new Date(Date.now()).valueOf()}`,
+      displayName: 'dev-testing',
+      image: ''
+    };
+  }
+  if (!user?.sub) throw new Error('No user found');
+  return {
+    title: formState.title,
+    description: formState.description,
+    creator: {
+      id: user.sub,
+      displayName: user.nickname || '',
+      image: user.picture
+    }
+  };
+};
