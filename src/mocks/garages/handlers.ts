09c7c99cefb1d0a7fa02d514d695a0f59a392cb8
James Plummer <plum_1990@hotmail.co.uk>
2022-06-24 08:01:05 +0100

    format response logic for dev  post garage endpoint

--- src/mocks/garages/handlers.ts
+++ src/mocks/garages/handlers.ts
@@ -1,6 +1,7 @@
 import { rest } from 'msw';
 import { applyGarageFilters } from '../../utils/dev-data/utils';
 import data from '../../utils/dev-data/garages.json';
+import { CreateGarageDataType, GarageDataType } from '../../types';
 
 export const garagesHandlers = [
   rest.get(
@@ -44,8 +45,39 @@ export const garagesHandlers = [
       process.env.NEXT_PUBLIC_API_BASE_URL ?? process.env.API_BASE_URL
     }/garages`,
     (req, res, ctx) => {
+      const garage = formatPostGarageResponse(req.body as CreateGarageDataType);
+
       // dev util. formats and returns input
-      return res(ctx.delay(), ctx.status(200), ctx.json(req.body));
+      return res(ctx.delay(), ctx.status(200), ctx.json(garage));
     }
   )
 ];
+
+const formatPostGarageResponse = (
+  newGarage: CreateGarageDataType
+): GarageDataType => {
+  const { imageFiles, ...garageData } = newGarage;
+  const drivers: string[] = [];
+  const liveries: string[] = [];
+
+  // create new garage entry in database (send garage data)
+  const id = `${new Date(Date.now()).valueOf()}`;
+  const now = new Date(Date.now()).valueOf();
+  const createdAt = now;
+  const updatedAt = now;
+
+  // add garageId to user
+
+  // upload images, link urls to garage
+  const image: string = '/car6.png'; // resulting urls from uploading the images
+
+  return {
+    ...garageData,
+    createdAt,
+    drivers,
+    id,
+    image,
+    liveries,
+    updatedAt
+  };
+};
