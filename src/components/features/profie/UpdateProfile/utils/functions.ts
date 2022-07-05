2f285ca7d1fa6573dd237390caaef16849989616
James Plummer <plum_1990@hotmail.co.uk>
2022-07-05 18:47:20 +0100

    added helper to map form state to upload format

---
+++ src/components/features/profie/UpdateProfile/utils/functions.ts
@@ -0,0 +1,34 @@
+import { UserProfile } from '@auth0/nextjs-auth0';
+
+import { FormStateType } from '../../../../shared/Form/types';
+import { UpdateProfileFormStateType } from '../types';
+import { UpdateUserProfileDataType } from '../../../../../types';
+
+interface MapUpdateProfileFormStateToRequestInputArgs {
+  formState: FormStateType<UpdateProfileFormStateType>;
+  user: UserProfile | undefined;
+}
+export const mapUpdateProfileFormStateToRequestInput = ({
+  formState,
+  user
+}: MapUpdateProfileFormStateToRequestInputArgs): Omit<
+  UpdateUserProfileDataType,
+  'imageFiles'
+> => {
+  if (process.env.NODE_ENV === 'development') {
+    user = {
+      sub: `${new Date(Date.now()).valueOf()}`,
+      displayName: 'dev-testing',
+      image: ''
+    };
+  }
+  if (!user?.sub) throw new Error('No user found');
+  return {
+    id: user.sub,
+    about: formState.about || '',
+    forename: formState.forename || '',
+    surname: formState.surname || '',
+    displayName: formState.displayName,
+    email: formState.email
+  };
+};
