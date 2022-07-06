bd6fc93cf39e0cac0dd539056a7966c1b1c8fc3f
James Plummer <plum_1990@hotmail.co.uk>
2022-07-06 08:50:11 +0100

    added msw mock for getUserById get

--- src/mocks/user/handlers.ts
+++ src/mocks/user/handlers.ts
@@ -1,5 +1,7 @@
 import { rest } from 'msw';
 import { UpdateUserProfileDataType, UserDataType } from '../../types';
+import userData from '../../utils/dev-data/users.json';
+import liveriesData from '../../utils/dev-data/liveries.json';
 
 export const userHandlers = [
   rest.put(
@@ -11,8 +13,32 @@ export const userHandlers = [
         req.body as UpdateUserProfileDataType
       );
 
-      // dev util. formats and returns input
       return res(ctx.delay(), ctx.status(200), ctx.json(response));
+    }
+  ),
+  rest.get(
+    `${
+      process.env.NEXT_PUBLIC_API_BASE_URL ?? process.env.API_BASE_URL
+    }/user/:id`,
+    (req, res, ctx) => {
+      const { id } = req.params;
+
+      const user = userData.find((el) => el.id === id);
+      if (!user) return res(ctx.delay(), ctx.status(400));
+
+      const { about, displayName, image } = user;
+
+      const liveries = liveriesData.reduce((prev, cur) => {
+        const output = [...prev];
+        if (cur.creator.id === id) output.push(cur.id);
+        return output;
+      }, [] as string[]);
+
+      return res(
+        ctx.delay(),
+        ctx.status(200),
+        ctx.json({ id, about, displayName, image, liveries })
+      );
     }
   )
 ];
