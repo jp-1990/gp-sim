5e31b133456441746a0771ad171732a4d48f4bac
James Plummer <plum_1990@hotmail.co.uk>
2022-10-20 18:47:11 +0100

    update post garages api route to interact with firebase

--- src/pages/api/v1/garages/index.ts
+++ src/pages/api/v1/garages/index.ts
@@ -1,5 +1,7 @@
+import formidable from 'formidable';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import {
+  CreateGarageDataType,
   GarageDataType,
   GaragesDataType,
   GaragesResponseType,
@@ -7,10 +9,18 @@ import {
 } from '../../../../types';
 import {
   Collection,
+  FieldValue,
   firestore,
   NextApiRequestWithAuth,
   withAuth
 } from '../../../../utils/firebase/admin';
+
+//set bodyparser
+export const config = {
+  api: {
+    bodyParser: false
+  }
+};
 
 async function handler(
   req: NextApiRequestWithAuth,
@@ -73,19 +83,77 @@ async function handler(
       }
     }
     case Method.POST: {
-      res.status(200).json([
-        {
-          id: '0',
-          createdAt: 0,
-          updatedAt: 0,
-          creator: { id: '0', displayName: 'name', image: '' },
-          title: 'created garage',
-          description: 'desc',
-          image: '',
-          drivers: [],
-          liveries: []
+      try {
+        // check isAuthenticated
+        if (!req.isAuthenticated || !req.uid) {
+          return res.status(401).json({ error: 'unauthorized' });
         }
-      ]);
+
+        // parse req
+        const parsedData = await new Promise<CreateGarageDataType>(
+          (resolve, reject) => {
+            const form = formidable({ multiples: true });
+            form.parse(req, (err, fields, files) => {
+              if (err) throw new Error(err);
+              try {
+                // check req body
+                const rawData = { ...fields, ...files } as Record<string, any>;
+                const properties = [
+                  'creator',
+                  'description',
+                  'imageFiles',
+                  'title'
+                ] as const;
+                for (const property of properties) {
+                  if (!rawData.hasOwnProperty(property)) {
+                    return res
+                      .status(400)
+                      .json({ error: 'malformed request body' });
+                  }
+                }
+                rawData.creator = JSON.parse(rawData.creator);
+
+                resolve(rawData as CreateGarageDataType);
+              } catch (err) {
+                reject(err);
+              }
+            });
+          }
+        );
+
+        const { imageFiles, ...data } = parsedData;
+        const timestamp = Date.now();
+        const newGarageRef = garagesRef.doc();
+        const newGarageData: GarageDataType = {
+          createdAt: timestamp,
+          drivers: [],
+          id: newGarageRef.id,
+          image: '',
+          liveries: [],
+          updatedAt: timestamp,
+          ...data
+        };
+
+        // TODO: upload files
+
+        // batch write create doc, increment count, add to user and garage
+        const batch = firestore.batch();
+        batch.set(newGarageRef, newGarageData);
+
+        // update user doc
+        const userId = newGarageData.creator.id;
+        const userRef = firestore.collection(Collection.USERS).doc(userId);
+        batch.update(userRef, {
+          garages: FieldValue.arrayUnion(newGarageRef.id)
+        });
+
+        // run batch
+        await batch.commit();
+
+        return res.status(200).json([newGarageData]);
+      } catch (err) {
+        return res.status(500).json({ error: 'internal error' });
+      }
     }
   }
 }
