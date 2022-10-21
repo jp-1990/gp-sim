b22f94a678859e6d0ad9da97a4745671ea71a711
James Plummer <plum_1990@hotmail.co.uk>
2022-10-21 07:17:20 +0100

    update get garages[id] api routes to interact with firebase

--- src/pages/api/v1/garages/[id].ts
+++ src/pages/api/v1/garages/[id].ts
@@ -1,52 +1,82 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
 import { GarageDataType, Method } from '../../../../types';
+import {
+  firestore,
+  NextApiResponse,
+  NextApiRequestWithAuth,
+  Collection,
+  withAuth
+} from '../../../../utils/firebase/admin';
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<GarageDataType>
+async function handler(
+  req: NextApiRequestWithAuth,
+  res: NextApiResponse<GarageDataType | { [key: string]: string }>
 ) {
   const method = req.method;
-  const query = req.query;
+  const garagesRef = firestore.collection(Collection.GARAGES);
 
   switch (method) {
     case Method.GET: {
-      res.status(200).json({
-        id: query.id as string,
-        createdAt: 0,
-        updatedAt: 0,
-        creator: { id: '0', displayName: 'name', image: '' },
-        title: 'get garage by id',
-        description: 'desc',
-        image: '',
-        drivers: [],
-        liveries: []
-      });
-    }
-    case Method.PATCH: {
-      res.status(200).json({
-        id: query.id as string,
-        createdAt: 0,
-        updatedAt: 0,
-        creator: { id: '0', displayName: 'name', image: '' },
-        title: 'update garage by id',
-        description: 'desc',
-        image: '',
-        drivers: [],
-        liveries: []
-      });
+      try {
+        // check isAuthenticated
+        if (!req.isAuthenticated || !req.uid) {
+          return res.status(401).json({ error: 'unauthorized' });
+        }
+
+        // check req params
+        const params = {
+          id: req.query.id as string | undefined
+        };
+
+        if (!params.id) {
+          return res.status(400).json({ error: 'malformed request params' });
+        }
+
+        const garageRef = garagesRef.doc(params.id);
+        const garage = (await garageRef.get()).data() as
+          | GarageDataType
+          | undefined;
+
+        // user can only access a garage if they own it or belong to it
+        if (
+          garage?.creator.id !== req.uid &&
+          !garage?.drivers.includes(req.uid)
+        ) {
+          return res.status(401).json({ error: 'unauthorized' });
+        }
+
+        return res.status(200).json(garage);
+      } catch (err) {
+        return res.status(500).json({ error: 'internal error' });
+      }
     }
     case Method.DELETE: {
-      res.status(200).json({
-        id: query.id as string,
-        createdAt: 0,
-        updatedAt: 0,
-        creator: { id: '0', displayName: 'name', image: '' },
-        title: 'delete garage by id',
-        description: 'desc',
-        image: '',
-        drivers: [],
-        liveries: []
-      });
+      // try {
+      //   // check isAuthenticated
+      //   if (!req.isAuthenticated || !req.uid) {
+      //     return res.status(401).json({ error: 'unauthorized' });
+      //   }
+      //   // check req params
+      //   const params = {
+      //     id: req.query.id as string | undefined
+      //   };
+      //   if (!params.id) {
+      //     return res.status(400).json({ error: 'malformed request params' });
+      //   }
+      //   const liveryRef = garagesRef.doc(params.id);
+      //   const livery = await liveryRef.get();
+      //   // delete only if creator id matches auth user id
+      //   if (livery.data()?.creator.id === req.uid) liveryRef.delete();
+      //   return res.status(200).json({ id: params.id });
+      // } catch (err) {
+      //   return res.status(500).json({ error: 'internal error' });
+      // }
+    }
+    default: {
+      return res
+        .status(501)
+        .json({ error: 'requested endpoint does not exist' });
     }
   }
 }
+
+export default withAuth(handler);
