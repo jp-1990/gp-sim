3529be5b317824c35683202e7856f0886671cccf
James Plummer <plum_1990@hotmail.co.uk>
2022-12-24 18:19:07 +0000

    update get all users api call to sort and filter

--- src/pages/api/v1/users/index.ts
+++ src/pages/api/v1/users/index.ts
@@ -1,4 +1,5 @@
 import { UsersDataType, Method, UserDataType } from '../../../../types';
+import { applyUserFilters } from '../../../../utils/filtering/user';
 import {
   Collection,
   firestore,
@@ -22,15 +23,67 @@ async function handler(
           return res.status(401).json({ error: 'unauthorized' });
         }
 
-        // get users
-        const snapshot = await usersRef.get();
-        const users: UsersDataType = [];
-        snapshot.forEach((doc) => {
-          const user = doc.data() as unknown as UserDataType;
-          users.push(user);
-        });
+        const params = {
+          created: (Array.isArray(req.query.created)
+            ? req.query.created[0]
+            : req.query.created || 'desc') as FirebaseFirestore.OrderByDirection
+        };
 
-        return res.status(200).json(users);
+        const ids =
+          (Array.isArray(req.query.ids) ? req.query.ids[0] : req.query.ids) ??
+          '';
+
+        const definedParams = (
+          Object.keys(params) as (keyof typeof params)[]
+        ).reduce((output, param) => {
+          if (!params[param]) return output;
+          output.push(param);
+          return output;
+        }, [] as string[]);
+
+        type Order = [string, FirebaseFirestore.OrderByDirection | undefined];
+        const orders: Record<string, Order> = {
+          created: ['createdAt', params.created]
+        };
+
+        // GET USERS
+        // if not filtering by ids, normal query
+        if (!ids.length) {
+          // prepare query
+          let query =
+            usersRef as FirebaseFirestore.Query<FirebaseFirestore.DocumentData>;
+          for (const param of definedParams) {
+            if (orders[param]) query = query.orderBy(...orders[param]);
+          }
+
+          const snapshot = await query.get();
+          const users: UsersDataType = [];
+          snapshot.forEach((doc) => {
+            const user = doc.data() as unknown as UserDataType;
+            users.push(user);
+          });
+          return res.status(200).json(users);
+        }
+
+        // if filtering by ids, fetch individualy and sort on server
+        if (ids.length) {
+          const users: UsersDataType = [];
+          for (const id of ids) {
+            const userDoc = await usersRef.doc(id).get();
+            const user = userDoc.data();
+            if (user) users.push(user as UserDataType);
+          }
+
+          const filters: Record<string, any> = {};
+          for (const param of definedParams) {
+            if (orders[param]) filters[param] = orders[param][1];
+          }
+          const sortedUsers = applyUserFilters(users, filters);
+
+          return res.status(200).json(sortedUsers);
+        }
+
+        return res.status(200).json([]);
       } catch (err) {
         return res.status(500).json({ error: 'internal error' });
       }
