83c4e719f815f6998691c603fcedfa3f9f5c36ce
James Plummer <plum_1990@hotmail.co.uk>
2022-12-24 16:33:35 +0000

    update liveries dev data. add tests for liveries filter function

--- src/utils/filtering/livery.test.ts
+++ src/utils/filtering/livery.test.ts
@@ -1,20 +1,90 @@
 import { applyLiveryFilters } from './livery';
+import { Order } from '../../types';
+
+import cars from '../dev-data/cars.json';
+import data from '../dev-data/liveries.json';
 
 describe('apply livery filters', () => {
   test('should correctly apply ids filter', () => {
-    return false;
+    const targetIds = `${data[0].id}&${data[1].id}&${data[2].id}`;
+    const filteredLiveries = applyLiveryFilters(data, { ids: targetIds });
+
+    expect(filteredLiveries).toHaveLength(3);
+
+    const targetIdsArray = targetIds.split('&');
+    for (const livery of filteredLiveries) {
+      expect(targetIdsArray.includes(livery.id)).toBeTruthy();
+    }
   });
   test('should correctly apply car filter', () => {
-    return false;
+    const car = cars[0].name;
+    const filteredLiveries = applyLiveryFilters(data, { car });
+
+    for (const livery of filteredLiveries) {
+      expect(livery.car).toEqual(car);
+    }
   });
   test('should correctly apply rating filter', () => {
-    return false;
+    const rating = 3;
+    const filteredLiveries = applyLiveryFilters(data, { rating: `${rating}` });
+
+    for (const livery of filteredLiveries) {
+      expect(livery.rating).toBeGreaterThanOrEqual(rating);
+    }
   });
   test('should correctly apply search filter', () => {
-    return false;
+    const search = 'PuRple tIgeR';
+    const searchTerms = search.toLowerCase().split(' ');
+
+    const filteredLiveries = applyLiveryFilters(data, { search });
+
+    for (const livery of filteredLiveries) {
+      expect(
+        livery.searchHelpers.some((e) => searchTerms.includes(e.toLowerCase()))
+      ).toBeTruthy();
+    }
   });
-  test('should correctly sort by created', () => {});
-  return false;
-  test('should correctly sort by rating', () => {});
-  test('should correctly apply limit and offset', () => {});
+  test('should correctly sort by created (asc)', () => {
+    const filteredLiveries = applyLiveryFilters(data, { created: Order.ASC });
+
+    let prevCreatedAt = filteredLiveries[0].createdAt;
+    for (let i = 1; i < filteredLiveries.length; i++) {
+      expect(filteredLiveries[i].createdAt).toBeLessThanOrEqual(prevCreatedAt);
+      prevCreatedAt = filteredLiveries[i].createdAt;
+    }
+  });
+  test('should correctly sort by created (desc)', () => {
+    const filteredLiveries = applyLiveryFilters(data, { created: Order.DESC });
+
+    let prevCreatedAt = filteredLiveries[0].createdAt;
+    for (let i = 1; i < filteredLiveries.length; i++) {
+      expect(filteredLiveries[i].createdAt).toBeGreaterThanOrEqual(
+        prevCreatedAt
+      );
+      prevCreatedAt = filteredLiveries[i].createdAt;
+    }
+  });
+  test('should correctly sort by rating', () => {
+    const rating = 3;
+    const filteredLiveries = applyLiveryFilters(data, { rating: `${rating}` });
+
+    let prevRating = filteredLiveries[0].rating;
+    for (let i = 1; i < filteredLiveries.length; i++) {
+      expect(filteredLiveries[i].rating).toBeGreaterThanOrEqual(rating);
+      expect(filteredLiveries[i].rating).toBeGreaterThanOrEqual(prevRating);
+      prevRating = filteredLiveries[i].rating;
+    }
+  });
+  test('should correctly apply limit and offset', () => {
+    const filteredLiveries = applyLiveryFilters(data, {});
+
+    expect(filteredLiveries).toHaveLength(12);
+
+    const lastLiveryId = filteredLiveries[filteredLiveries.length - 1].id;
+
+    const offsetFilteredLiveries = applyLiveryFilters(data, { lastLiveryId });
+
+    expect(offsetFilteredLiveries).toHaveLength(12);
+    expect(offsetFilteredLiveries[0].id).not.toEqual(lastLiveryId);
+  });
 });
